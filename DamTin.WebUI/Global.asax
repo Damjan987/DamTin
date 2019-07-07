096bc3c18c632ed8500cffbe34b94c7c035aaac4
Damjan <jasamprogramer7@gmail.com>
2019-07-07 11:07:44 +0100

    Initial Solution Setup

---
+++ DamTin.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DamTin.WebUI.MvcApplication" Language="C#" %>
---
+++ DamTin.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DamTin.WebUI.Startup))]
+namespace DamTin.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
